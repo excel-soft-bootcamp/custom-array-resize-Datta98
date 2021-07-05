d0b68f5c2b870a45c65b5af306e6c3b423c50663
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-05 16:13:35 +0530

    Add files via upload

---
+++ ArrayUtilityApplication/ArrayUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayUtilityApplication
+{
+    public static class ArrayUtility
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+
+            int[] newarray = new int[newSize];
+            Array.Copy(array, newarray, array.Length);
+            array = newarray;
+
+           
+
+        }
+
+    }
+
+}
